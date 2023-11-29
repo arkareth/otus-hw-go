3900c0dfe0c4e14cc66e822d78227f3822321da4
azltv <arkareth@gmail.com>
2023-11-29 12:02:39 +0700

    Moved run into the main func body

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -1,9 +1,5 @@
 package hw06pipelineexecution
 
-import (
-	"time"
-)
-
 type (
 	In  = <-chan interface{}
 	Out = In
@@ -16,38 +12,28 @@ func ExecutePipeline(in In, done In, stages ...Stage) Out {
 	if len(stages) == 0 {
 		return nil
 	}
-	out := make(Out)
 
+	var out Bi
 	for _, s := range stages {
 		select {
 		case <-done:
-			return out
+			return nil
 		default:
-			// Sleep to accommodate the termination case
-			// Otherwise, launches an extra pipeline stage sometimes
-			// Which doesn't fit the test's tolerances
-			time.Sleep(time.Millisecond * 10)
-			out = run(in, done, s)
+			out = make(Bi)
+			go func(in In, out Bi, s Stage) {
+				defer close(out)
+				for v := range s(in) {
+					select {
+					case <-done:
+						return
+					default:
+						out <- v
+					}
+				}
+			}(in, out, s)
 			in = out
 		}
 	}
-
-	return out
-}
-
-func run(in In, done In, stage Stage) Out {
-	out := make(Bi)
-	go func(in In, done In, stage Stage) {
-		defer close(out)
-		for v := range stage(in) {
-			select {
-			case <-done:
-				return
-			default:
-				out <- v
-			}
-		}
-	}(in, done, stage)
 
 	return out
 }
